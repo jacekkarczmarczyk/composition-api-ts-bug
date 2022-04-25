66b0a7d3d9149b6b489257b466f57b4440e90cf3
Jacek Karczmarczyk <jk@drukcd.pl>
2022-04-25 11:55:31 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref, watch } from '@vue/composition-api';
+
+const foo = ref('');
+watch([foo] as const, ([foo]) => {
+  console.log(foo.length);
+})
+</script>
+
+<template>
+  <div id="app" />
+</template>
